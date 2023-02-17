7cc783fb902e4b232154a09c275ef022b0b927b5
khayo <khayovannucci@gmail.com>
2023-02-17 19:00:11 -0300

    Add project files.

---
+++ InventoryAPI/Services/ProductCatalogService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+
+namespace InventoryAPI.Services
+{
+    public class ProductCatalogService : ProductCatalog.ProductCatalogBase
+    {
+        public override Task<ProductDetailsReply> GetProductDetails(
+            ProductDetailsRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new ProductDetailsReply
+            {
+                Id = request.Id,
+                Name = "Purple Bowtie",
+                Sku = "purbow",
+                Price = new Price
+                {
+                    Amount = 100,
+                    CurrencyCode = "USD"
+                }
+
+            });
+        }
+    }
+}
---
+++ InventoryClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using Grpc.Net.Client;
+using InventoryAPI;
+using System.Text.Json;
+//using Product;
+
+Console.WriteLine("Teste gRPC");
+
+var channel = GrpcChannel.ForAddress("https://localhost:7117");
+var client = new ProductCatalog.ProductCatalogClient(channel); ;
+
+var request = new ProductDetailsRequest
+{
+    Id = 1,
+};
+
+var response = await client.GetProductDetailsAsync(request);
+
+Console.WriteLine(JsonSerializer.Serialize(response, new JsonSerializerOptions
+{
+    WriteIndented = true
+}));
+
+Console.ReadKey();
